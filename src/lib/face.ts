d0894629f3b331043fe05e5f2c7e97af039cd86d
IRHM <37304121+IRHM@users.noreply.github.com>
2022-08-07 16:33:06 +0100

    Add try catches around both funcs in face.ts
    So we can give the user a "real" error and disable components on table msg. The error could be caused by wrongly defined customIds on buttons, so best to have the rando run whole command again?

--- src/lib/face.ts
+++ src/lib/face.ts
@@ -11,74 +11,88 @@ import API from "./api";
 import { makeATable } from "./helpMe";
 
 export async function listContent(api: API, interaction: ChatInputCommandInteraction) {
-  const name = interaction.options.getString("name");
-  const quality = interaction.options.getString("quality");
+  try {
+    const name = interaction.options.getString("name");
+    const quality = interaction.options.getString("quality");
 
-  if (!name || !quality) {
-    console.error("Attempted to run command without name or quality args.");
-    console.error("----> name:", name, "quality id:", quality);
-    return;
+    if (!name || !quality) {
+      console.error("Attempted to run command without name or quality args.");
+      console.error("----> name:", name, "quality id:", quality);
+      return;
+    }
+
+    const content = (await api.search(name)).splice(0, 5);
+    const buttons = [];
+
+    if (!content || content.length <= 0) {
+      interaction.reply(`Couldn't find any content matching ${name}`);
+      return;
+    }
+
+    for (const _k in content) {
+      const k = Number(_k);
+      const show = content[k];
+      buttons.push(
+        new ButtonBuilder()
+          .setCustomId(`${api.type === "radarr" ? "movie" : "series"}:${show.imdbId}:${quality}`)
+          .setEmoji(`${NUMBER_EMOJIS[k]}`)
+          .setStyle(k == 0 ? ButtonStyle.Primary : ButtonStyle.Secondary)
+      );
+    }
+
+    const table = makeATable(content);
+
+    interaction.reply({
+      content: "```" + table + "```",
+      components: [new ActionRowBuilder<ButtonBuilder>().addComponents(...buttons)]
+    });
+  } catch (err) {
+    console.error("Encountered an error trying to listContent:", err);
+    interaction.reply({ content: "Errored whilst attempting to list searched content.", ephemeral: true });
   }
-
-  const content = (await api.search(name)).splice(0, 5);
-  const buttons = [];
-
-  if (!content || content.length <= 0) {
-    interaction.reply(`Couldn't find any content matching ${name}`);
-    return;
-  }
-
-  for (const _k in content) {
-    const k = Number(_k);
-    const show = content[k];
-    buttons.push(
-      new ButtonBuilder()
-        .setCustomId(`${api.type === "radarr" ? "movie" : "series"}:${show.imdbId}:${quality}`)
-        .setEmoji(`${NUMBER_EMOJIS[k]}`)
-        .setStyle(k == 0 ? ButtonStyle.Primary : ButtonStyle.Secondary)
-    );
-  }
-
-  const table = makeATable(content);
-
-  interaction.reply({
-    content: "```" + table + "```",
-    components: [new ActionRowBuilder<ButtonBuilder>().addComponents(...buttons)]
-  });
 }
 
 export async function addContent(api: API, interaction: ButtonInteraction, args: string[]) {
-  const { 0: imdbId, 1: qualityId } = args;
-  console.log("button reached", imdbId, qualityId);
+  try {
+    const { 0: imdbId, 1: qualityId } = args;
+    console.log("button reached", imdbId, qualityId);
 
-  if (!imdbId || !qualityId) return;
+    if (!imdbId || !qualityId) return;
 
-  const content = await api.add(imdbId, qualityId);
-  let poster = content.images.find((e: any) => e.coverType === "poster")?.remoteUrl;
-  if (!poster && content?.images && content.images[0]?.remoteUrl) {
-    poster = content.images[0]?.remoteUrl;
+    const content = await api.add(imdbId, qualityId);
+    let poster = content.images.find((e: any) => e.coverType === "poster")?.remoteUrl;
+    if (!poster && content?.images && content.images[0]?.remoteUrl) {
+      poster = content.images[0]?.remoteUrl;
+    }
+
+    interaction.message.edit({
+      embeds: [
+        new EmbedBuilder()
+          .setTitle(`${content.title}`)
+          .addFields(
+            { name: "Year", value: `${content.year}`, inline: true },
+            { name: "IMDb", value: `[Link](https://www.imdb.com/title/${content.imdbId})`, inline: true },
+
+            api.type === "sonarr"
+              ? { name: "TVDB", value: `[Link](https://thetvdb.com/?tab=series&id=${content.tvdbId})`, inline: true }
+              : { name: "TMDB", value: `[Link](https://www.themoviedb.org/movie/${content.tmdbId})`, inline: true },
+
+            { name: "Genres", value: `${content.genres.join(", ")}`, inline: false }
+            // { name: "Overview", value: `${content.overview.slice(0, 100)}...`, inline: false }
+          )
+          .setImage(poster)
+          .setTimestamp()
+          .setFooter({ text: "Search started" })
+      ],
+      content: "",
+      components: [] // Empty arr to remove existing buttons
+    });
+  } catch (err) {
+    console.error("Encountered an error trying to addContent:", err);
+    interaction.message.edit({ components: [] });
+    interaction.reply({
+      content: "Errored whilst adding the selected content. Try running your command again plos.",
+      ephemeral: true
+    });
   }
-
-  interaction.message.edit({
-    embeds: [
-      new EmbedBuilder()
-        .setTitle(`${content.title}`)
-        .addFields(
-          { name: "Year", value: `${content.year}`, inline: true },
-          { name: "IMDb", value: `[Link](https://www.imdb.com/title/${content.imdbId})`, inline: true },
-
-          api.type === "sonarr"
-            ? { name: "TVDB", value: `[Link](https://thetvdb.com/?tab=series&id=${content.tvdbId})`, inline: true }
-            : { name: "TMDB", value: `[Link](https://www.themoviedb.org/movie/${content.tmdbId})`, inline: true },
-
-          { name: "Genres", value: `${content.genres.join(", ")}`, inline: false }
-          // { name: "Overview", value: `${content.overview.slice(0, 100)}...`, inline: false }
-        )
-        .setImage(poster)
-        .setTimestamp()
-        .setFooter({ text: "Search started" })
-    ],
-    content: "",
-    components: [] // Empty arr to remove existing buttons
-  });
 }
